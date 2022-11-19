4e55adc8a85eb509306bd8f386926d9b5c4b5b6c
Teeradon <teeradon.taen@bumail.net>
2022-11-19 21:36:51 +0700

    LAB2

---
+++ LAB2EX1.py
@@ -0,0 +1,9 @@
+arr = ["Number", "ID", "Name", "Count"]
+lenarr = len(arr)
+index = arr.index("Name")
+
+print(arr)
+print(lenarr)
+print(index)
+arr.append("Status")
+print(arr)
---
+++ LAB2EX3.py
@@ -0,0 +1,4 @@
+arr = [5, 7, 9, 11, 13]
+print("print array = ",arr)
+print("length array = ",len(arr))
+print("summation array = ",sum(arr))
